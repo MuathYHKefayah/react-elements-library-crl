79f5adee8d22bfed23555b395e11f750c95fc08c
Moath Kefayah <moath.kefayah@hp.com>
2023-10-17 15:09:00 +0300

    Updates

---
+++ precache-manifest.12f14a05cef5e66d6a22effaaae6e46e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258817cd7c43e97fa400d2cd36467756",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54f59954ab1ffe055415",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "1b41fc76d2a460f640c6",
+    "url": "./static/js/2.bf9b02d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bf9b02d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f59954ab1ffe055415",
+    "url": "./static/js/main.dea93e97.chunk.js"
+  },
+  {
+    "revision": "6be7d22acd2ab1ad53ff",
+    "url": "./static/js/runtime-main.84f2e5e5.js"
+  }
+]);
